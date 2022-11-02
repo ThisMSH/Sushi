3654e26f38e5e3d323580b36d81e652d1b83eda0
ThisMSH <midoyugi@hotmail.com>
2022-11-02 13:59:16 +0100

    Version 1.2

--- js/script.js
+++ js/script.js
@@ -27,6 +27,23 @@ function ready() {
 	};
 	// End of header part
 
+	// Go to Top button
+	// Get the button:
+	let mybutton = document.getElementById("myTopBtn");
+
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			mybutton.style.display = "block";
+		} else {
+			mybutton.style.display = "none";
+		}
+	}
+
+	// End of Go to Top button
+
 	var removeCartItemsButton = document.getElementsByClassName("remove-button")
 	for (var i = 0; i < removeCartItemsButton.length; i++) {
 		var button = removeCartItemsButton[i]
@@ -124,4 +141,10 @@ function category(evt, categoryID) {
 
 	document.getElementById(categoryID).style.display = "flex"
 	evt.currentTarget.className += " active"
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
